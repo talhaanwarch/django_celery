beedd2554542b289b024017ca2c7e3f4b4e5b2d6
Talha Anwar <chtalhaanwar@gmail.com>
2024-02-25 17:14:38 +0500

    add celery task

--- app_celery/views.py
+++ app_celery/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from time import sleep
+from celery import shared_task
+
+@shared_task()
+def perform_task(input_text):
+	sleep(60)
+	return input_text
+
 def home(request):
 	if request.method == 'POST':
 		input_text = request.POST['input']
-		sleep(60)
+		input_text= perform_task.delay(input_text)
 		context = {'output_text':input_text}
 	else:
 		context = {}
